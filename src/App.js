64d32c8d390a64d5b8c6a581089e2063cdab47eb
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-12 11:48:53 +0200

    Add header, channel, message and message-input components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Header from './containers/header';
+import Channel from './containers/channel';
+import MessageInput from './containers/message-input';
 
 class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        );
-    }
+  constructor() {
+    super();
+
+    this.state = {
+      messages: [],
+      currentUser: "guest"
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Header appName="SLAKO" />
+        <Channel messages={ this.state.messages } />
+        <MessageInput username={ this.state.currentUser } />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/channel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Message from './message'
+
+class Channel extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.messages.map((message) => {
+          return <Message { ...message } />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default Channel;
